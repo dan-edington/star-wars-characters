9d75741e9907458aad0596df8670d47d73a8c9eb
Dan Edington <dan.edington@deptagency.com>
2021-07-12 17:08:54 +0100

    initial commit

---
+++ src/styles/fontFaces.ts
@@ -0,0 +1,13 @@
+import { css } from 'styled-components';
+
+const fontFaces = css`
+	@font-face {
+		font-family: 'Star Jedi';
+		font-style: normal;
+		font-weight: 400;
+		font-display: swap;
+		src: url('./assets/fonts/StarJedi.woff2') format('woff2');
+	}
+`;
+
+export default fontFaces;
